campg2j003/jfw_nsh

commit 6be7b258e515ba88c3c7191fc5e89b857acec7b3

Moved lang includes, basically works.
JFW.nsh: Moved message files after MUI_LANGUAGE macro invocations.

This works except for LangStrings in uninstlog.nsh which are not localized yet.

Sent installer to Fernando 12/24/15.

There remain a few strings that need to be localized.

JFW.nsh
@@ -591,7 +591,7 @@ insttype /COMPONENTSONLYONCUSTOM
 ;!define MUI_COMPONENTSPAGE_TEXT_INSTTYPE text
 ;!define MUI_COMPONENTSPAGE_TEXT_DESCRIPTION_TITLE text
 ;!define MUI_COMPONENTSPAGE_TEXT_DESCRIPTION_INFO text ;Text to display inside the description box when no section is selected.
-!define MUI_PAGE_CUSTOMFUNCTION_LEAVE ComponentsPageLeave
+!define MUI_PAGE_CUSTOMFUfNCTION_LEAVE ComponentsPageLeave
 !insertmacro mui_page_Components
 
 function ComponentsPageLeave
@@ -1432,7 +1432,7 @@ pop $0
 ;${stack::ns_size} $STACKSIZE ; debug
 ;DetailPrint "After JAWSInstallVersion stack size = $STACKSIZE" ; debug
 pop $1
-pop $0 ; restore version
+pop $0 ; restore version/lang
 StrCpy $UninstLogAlwaysLog ""
 !EndIf ;ifndef JAWSDEBUG
 ;${stack::ns_size} $STACKSIZE ; debug
@@ -1520,7 +1520,6 @@ SectionEnd
 !echo "VERSION='${VERSION}'"
 
 ;Language string files.  They are here because they have to come after the definition of ${VERSION}.
-!include "JFW_lang_enu.nsh" ;English language strings for this file
 
 
 ;The registry key in HKLM where the uninstall information is stored.
@@ -1584,6 +1583,9 @@ FunctionEnd ; InstFilesLeave
 ;  !insertmacro MUI_UNPAGE_COMPONENTS
   !insertmacro MUI_UNPAGE_INSTFILES
   !insertmacro MUI_LANGUAGE "English"
+  !insertmacro MUI_LANGUAGE "Spanish"
+!include "JFW_lang_enu.nsh" ;English language strings for this file
+!include "JFW_lang_esn.nsh" ;Spanish language strings for this file
 
 Function .OnInit
   ;Find where the JAWS program files are located.

commit 14f386813dce3b4c288aa50adfc75e31fab31f01

Removed typo "f" from definition of MUI_PAGE_CUSTOMFUNCTION_LEAVE introduced in 6be7b25 (12/25/15) that caused ComponentsPageLeave not to be referenced.

JFW.nsh
@@ -594,7 +594,7 @@ insttype /COMPONENTSONLYONCUSTOM
 ;!define MUI_COMPONENTSPAGE_TEXT_INSTTYPE text
 ;!define MUI_COMPONENTSPAGE_TEXT_DESCRIPTION_TITLE text
 ;!define MUI_COMPONENTSPAGE_TEXT_DESCRIPTION_INFO text ;Text to display inside the description box when no section is selected.
-!define MUI_PAGE_CUSTOMFUfNCTION_LEAVE ComponentsPageLeave
+!define MUI_PAGE_CUSTOMFUNCTION_LEAVE ComponentsPageLeave
 !insertmacro mui_page_Components
 
 function ComponentsPageLeave